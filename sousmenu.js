708d272b8683ccad717718b1fb1b6b7ab8c92dbe
axb157030 <amine.benelbar.1@gmail.com>
2019-03-20 13:45:18 -0500

    Add files via upload

---
+++ sousmenu.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	var posWhatSous;
+	var posIndex;
+	var sousIndex;
+	var id;
+	sousIndex = document.getElementById("sousI");
+		//posWhatSous = $("#whatSous").offset();
+	   posIndex = $("#sousI").offset();
+		var height = $(window).scrollTop();
+		//console.log("height " + height);
+
+
+
+	$(window).scroll(function() {
+
+		var height = $(window).scrollTop();
+		posIndex = $("#sousI").offset();
+		//console.log("height2 " + height);
+		var fontPx = 24;
+		
+		
+			
+			//console.log("fontSize rv " + posIndex.top);
+
+		if(posIndex.top > 200)
+		{
+			sousIndex.style.fontSize = "1em";
+			sousIndex.style.transition = "font-size 1s"; // Works fantastic! setinterval should only be used if this does not work and really only for on click functions
+			//console.log("fontSize rv ");
+		}
+		
+		else
+		{
+			sousIndex.style.fontSize = "1.5em";
+			
+		}
+
+	    
+});
+
+
+});
